8738d51d073ad64fb51e187ff18a729d3708bb0a
soreyeb <soreyeb@sam.cs.hendrix.edu>
2015-03-17 18:44:31 -0500

    implemented chat

--- Peer2PeerII/src/application/Controller.java
+++ Peer2PeerII/src/application/Controller.java
@@ -1,16 +1,6 @@
 package application;
 
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.concurrent.ArrayBlockingQueue;
-
-import javax.swing.BorderFactory;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-
-import sockdemo.TalkThread;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -25,8 +15,6 @@ import javafx.scene.layout.VBox;
 public class Controller {
 	
 	private MessageModel model = new MessageModel();
-	private sockdemo.TalkThread talker;
-	private ArrayBlockingQueue<String> channel;
 	
 	@FXML
 	TextArea messageTextArea;
@@ -71,40 +59,9 @@ public class Controller {
 	
 	@FXML
 	private void sendNewMessage(){
-		send(messageTextArea.getText(), "sam", 8888);
+		model.send(messageTextArea.getText(), "sam", 8888);
 		messageTextArea.setText("");
 	}
 	
-	private void send(String msg, String host, int port) {
-		if (talker != null && talker.isGoing()) {
-			talker.halt();
-		}
-		talker = new TalkThread(msg, host, port, channel);
-		new Receiver().start();
-		talker.start();		
-	}
 	
-	/*private JTextArea makeTextArea(String title, JPanel where) {
-		JTextArea t = new JTextArea();
-		JScrollPane scroller = new JScrollPane(t);
-		scroller.setBorder(BorderFactory.createTitledBorder(title));
-		where.add(scroller);
-		return t;
-	}*/
-	
-	
-	private class Receiver extends Thread {
-		public void run() {
-			while (talker.isGoing()) {
-				String line;
-				try {
-					line = channel.take();
-					model.addMessage(line);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}
-	}
 }
--- Peer2PeerII/src/application/MessageModel.java
+++ Peer2PeerII/src/application/MessageModel.java
@@ -1,14 +1,44 @@
 package application;
 
+import java.util.concurrent.ArrayBlockingQueue;
+
+import sockdemo.TalkThread;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class MessageModel {
 	private ObservableList<Message> messages = FXCollections.observableArrayList();
 	ObservableList<Message> getObservable() {return messages;}
+	sockdemo.TalkThread talker;
+	private ArrayBlockingQueue<String> channel = new ArrayBlockingQueue<String>(2, true);
 	
 	public void addMessage(String text){
 		Message message = new Message(text);
 		messages.add(message);
+	}
+	
+	public void send(String msg, String host, int port) {
+		if (talker != null && talker.isGoing()) {
+			talker.halt();
+		}
+		talker = new TalkThread(msg, host, port, channel);
+		new Receiver().start();
+		talker.start();		
+	}
+		
+	
+	public class Receiver extends Thread {
+		public void run() {
+			while (talker.isGoing()) {
+				String line;
+				try {
+					line = channel.take();
+					addMessage(line);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
